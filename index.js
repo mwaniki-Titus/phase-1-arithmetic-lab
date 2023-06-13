b355c5fa6326449a4015bee7a9c760301f651f19
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-13 16:36:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+const multiply=num1*num2;    
+console.log(multiply);
+let random = Math.ceil(Math.random() * 10) + 1;
+const num3=28
+const num4=6
+const mod=num3%num4
+
+const set=[0,6,9,1,13,12,20,]
+const max=Math.max(...set)
+console.log(max);
